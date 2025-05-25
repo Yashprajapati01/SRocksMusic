1a29782f6fbe7e168d413ea3303ac9d940be7954
Yash <Prajapatiyash0011@gmail.com>
2025-05-26 00:00:18 +0530

    Improved the border colors and width

--- lib/widgets/service_card.dart
+++ lib/widgets/service_card.dart
@@ -11,60 +11,76 @@ class ServiceTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(vertical: 8.h, horizontal: 30.w),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(15.r),
-        child: Container(
-          height: 70.h,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(service.backgroundImage),
-              fit: BoxFit.cover,
-            ),
-          ),
-          child: Container(
-            decoration: BoxDecoration(
-              color: Color.fromRGBO(32, 33, 38, 0.85),
-            ),
-            child: Padding(
-              padding: EdgeInsets.all(12.w),
-              child: Row(
-                children: [
-                  Image.asset(service.icon, fit: BoxFit.contain),
-                  SizedBox(width: 10.w),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          service.title,
-                          style: TextStyle(
-                            fontFamily: 'Syne',
-                            fontWeight: FontWeight.w700,
-                            fontSize: 15.sp,
-                            color: Colors.white,
-                          ),
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 1,
-                        ),
-                        Text(
-                          service.description,
-                          style: TextStyle(
-                            fontFamily: 'Syne',
-                            fontWeight: FontWeight.w400,
-                            fontSize: 13.sp,
-                            color: Colors.white,
-                          ),
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 1,
-                        ),
-                      ],
-                    ),
-                  ),
-                  const Icon(Icons.arrow_right, color: Colors.white, size: 33),
-                ],
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Color.fromRGBO(44, 45, 49, 1), width: 1.w),
+          borderRadius: BorderRadius.circular(15.r),
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(15.r),
+          child: Stack(
+            children: [
+              // Background image
+              Positioned.fill(
+                child: Image.asset(
+                  service.backgroundImage,
+                  fit: BoxFit.cover,
+                ),
               ),
-            ),
+
+              // Semi-transparent overlay with rounded corners
+              Positioned.fill(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(32, 33, 38, 0.85),
+                    borderRadius: BorderRadius.circular(15.r),
+                  ),
+                ),
+              ),
+
+              // Content
+              Container(
+                height: 70.h,
+                padding: EdgeInsets.all(12.w),
+                child: Row(
+                  children: [
+                    Image.asset(service.icon, fit: BoxFit.contain),
+                    SizedBox(width: 10.w),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            service.title,
+                            style: TextStyle(
+                              fontFamily: 'Syne',
+                              fontWeight: FontWeight.w700,
+                              fontSize: 15.sp,
+                              color: Colors.white,
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 1,
+                          ),
+                          Text(
+                            service.description,
+                            style: TextStyle(
+                              fontFamily: 'Syne',
+                              fontWeight: FontWeight.w400,
+                              fontSize: 13.sp,
+                              color: Colors.white,
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 1,
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Icon(Icons.arrow_right, color: Colors.white, size: 33),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),
